99a3c3df44d48c13ae1b85de7ed3fe57e85d1699
Glenn Vandeuren <vandeurenglenn@gmail.com>
2019-03-08 00:15:11 +0100

    :metal: main: setupLibrary and constants

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import { launch } from 'carlo';
 import { homedir } from 'os';
 import { join } from 'path';
+import { promisify } from 'util';
+import { readdir as _readdir, mkdir as _mkdir, writeFile, readFile } from 'fs';
+// import setupLibrary from '.lib/setup-library.js';
+import { DEFAULT_LIBRARY, LIBRARY, USER_DATA, LIBRARY_VERSION } from './constants';
 
-const userDataDir = join(homedir(), '.daw');
+const readdir = promisify(_readdir);
+const write = promisify(writeFile);
+const read = promisify(readFile);
+const mkdir = promisify(_mkdir);
+
+const setupLibrary = async () => {
+  await mkdir(LIBRARY);
+  await write(join(LIBRARY, 'version'), LIBRARY_VERSION);
+}
+
+const library = async () => {
+  const lib = await readdir(DEFAULT_LIBRARY);
+  const userLib = await readdir(LIBRARY);
+  return [lib, userLib];
+}
 
 (async () => {
 
@@ -10,7 +28,7 @@ const userDataDir = join(homedir(), '.daw');
     bgcolor: '#fff',
     width: 1400,
     height: 840,
-    userDataDir,
+    userDataDir: USER_DATA,
     title: 'DAW'
   });
 
@@ -24,7 +42,13 @@ const userDataDir = join(homedir(), '.daw');
   app.on('window', window => {
     window.load('index.html')
   });
-
-  // await app.exposeFunction('readdir', readdir);
+  try {
+    // TODO: listen for changes with chokidar ...
+    await read(join(LIBRARY, 'version'));
+  } catch (e) {
+    if (e.code === 'ENOENT') await setupLibrary();
+  }
+  await app.exposeFunction('readdir', readdir);
+  await app.exposeFunction('library', library);
   await app.load('index.html');
 })();
